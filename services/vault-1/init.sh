a712e6808c57361f73a4bc483acfa302f8ea5739
Huy-DNA <huydo862003@gmail.com>
2025-05-06 16:41:22 +0700

    fix: issues when adding root account

--- services/vault-1/init.sh
+++ services/vault-1/init.sh
@@ -4,8 +4,17 @@ set -e
 source /vault/config/.env
 vault operator init > /vault/data/init.txt
 
+VAULT_TOKEN=$(grep 'Initial Root Token' /vault/data/init.txt | awk '{print $NF}')
+vault login $VAULT_TOKEN
+
 vault auth enable userpass
+
+vault policy write admin - <<EOL
+path "*" {
+  capabilities = ["create", "read", "update", "delete", "list", "sudo"]
+}
+EOL
 
 vault write auth/userpass/users/root \
   password=$ROOT_PASSWORD \
-  policies=root
+  policies=admin
