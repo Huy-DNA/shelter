220f6e90b8fb09cd971f9c5170ca25939dbd6132
Huy-DNA <huydo862003@gmail.com>
2025-05-06 15:20:08 +0700

    fix: enable userpass

--- services/vault-1/init.sh
+++ services/vault-1/init.sh
@@ -3,6 +3,12 @@ set -e
 
 vault operator init > /vault/data/init.txt
 
+ROOT_TOKEN=$(grep 'Initial Root Token' /vault/data/init.txt | awk '{print $NF}')
+
+vault login $ROOT_TOKEN
+
+vault auth enable userpass
+
 vault write auth/userpass/users/root \
-    password=$ROOT_PASSWORD \
-    policies=root
+  password=$ROOT_PASSWORD \
+  policies=root
