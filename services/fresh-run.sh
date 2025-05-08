626a0ea4a2c426d069a5ccacf1d2eb9a12d57ff8
Huy-DNA <huydo862003@gmail.com>
2025-05-08 11:46:38 +0700

    fix: properly clean vault_grafana-data

--- services/fresh-run.sh
+++ services/fresh-run.sh
@@ -9,7 +9,7 @@ for IMAGE in vault-1 vault-2 vault-3 vault-transit-1 nginx; do
   docker rm $(docker ps -a --filter ancestor=$IMAGE --format="{{.ID}}") --force 2>/dev/null || echo "Containers for $IMAGE not found"
 done
 
-docker volume rm vault_vault-1-data vault_vault-2-data vault_vault-3-data vault_vault-transit-1-data grafana-data vault_nginx-data || echo "Volumes not created yet"
+docker volume rm vault_vault-1-data vault_vault-2-data vault_vault-3-data vault_vault-transit-1-data grafana-data vault_nginx-data vault_grafana-data || echo "Volumes not created yet"
 
 docker rmi vault-1 vault-2 vault-3 vault-transit-1 nginx --force || echo "Images not created yet"
 
