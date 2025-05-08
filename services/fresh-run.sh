1681998183df428cb9a832bdf5b34405daf2f3b4
Huy-DNA <huydo862003@gmail.com>
2025-05-08 13:59:14 +0700

    fix: properly clean load balancer

--- services/fresh-run.sh
+++ services/fresh-run.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "🧹 Cleaning up old Docker services, containers, volumes, images..."
 
-docker service rm vault_vault-1 vault_vault-2 vault_vault-3 vault_vault-transit-1 vault_prometheus vault_grafana vault_nginx || echo "Services not created yet"
+docker service rm vault_vault-1 vault_vault-2 vault_vault-3 vault_vault-transit-1 vault_prometheus vault_grafana vault_load-balancer || echo "Services not created yet"
 
 for IMAGE in vault-1 vault-2 vault-3 vault-transit-1 load-balancer; do
   docker rm $(docker ps -a --filter ancestor=$IMAGE --format="{{.ID}}") --force 2>/dev/null || echo "Containers for $IMAGE not found"
